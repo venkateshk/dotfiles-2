98b9ec17cad8cd6d8f722a967383decf668594e1
Neeraj Singh <neerajdotname@gmail.com>
2014-08-11 10:20:33 -0400

    aliases changes

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -40,27 +40,17 @@ alias b="bundle"
 alias be="bundle exec"
 alias biv="bundle install --path vendor"
 
-function is {
-  INSTAPUSHER_HOST=107.170.11.75 API_KEY=9f2cae3d9a9cc8a4a1c0609604c703f7 /Users/nsingh/code/instapusher_project/instapusher/bin/instapusher --debug
+function inet {
+  echo "INSTAPUSHER_HOST=www.instapusher.net /Users/nsingh/code/instapusher_project/instapusher/bin/instapusher --debug"
+  INSTAPUSHER_HOST=www.instapusher.net /Users/nsingh/code/instapusher_project/instapusher/bin/instapusher --debug
 }
-
 
 function i2 {
   INSTAPUSHER_HOST=107.170.11.75 API_KEY=9f2cae3d9a9cc8a4a1c0609604c703f7 /Users/nsingh/code/instapusher_project/instapusher2/bin/instapusher2 --debug
 }
 
-
-
-# UTILS
-alias dns_flush='dscacheutil -flushcache'
-
 function it {
   echo "API_KEY=xxx /Users/nsingh/code/instapusher_project/instapusher/bin/instapusher --local --debug"
   API_KEY=xxx /Users/nsingh/code/instapusher_project/instapusher/bin/instapusher --local --debug
-}
-
-function inet {
-  echo "INSTAPUSHER_HOST=www.instapusher.net /Users/nsingh/code/instapusher_project/instapusher/bin/instapusher --debug"
-  INSTAPUSHER_HOST=www.instapusher.net /Users/nsingh/code/instapusher_project/instapusher/bin/instapusher --debug
 }
 
